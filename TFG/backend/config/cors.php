7ee5e6b82792701b7cc9897f0b89785516558168
ahersan1203 <ahersan1203@g.educaand.es>
2026-06-03 18:33:11 +0200

    Despliege9

--- TFG/backend/config/cors.php
+++ TFG/backend/config/cors.php
@@ -6,7 +6,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => [
+        'https://tfg-zona-gamer-o9uj.vercel.app'
+    ],
 
     'allowed_headers' => ['*'],
 
