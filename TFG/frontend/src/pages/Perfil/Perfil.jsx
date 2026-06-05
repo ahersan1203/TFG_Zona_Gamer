ef57ab4c57658491c18b59bff2c1550e32dba2b5
ahersan1203 <ahersan1203@g.educaand.es>
2026-06-05 10:51:22 +0200

    css perfil

--- TFG/frontend/src/pages/Perfil/Perfil.jsx
+++ TFG/frontend/src/pages/Perfil/Perfil.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import api from "../../api";
-import "./Perfil.css";
+import "./perfil.css";
 
 export default function Perfil() {
     const [user, setUser] = useState(null);
